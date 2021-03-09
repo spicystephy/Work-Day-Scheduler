4c17ed18eb1f2e3dc42ee2c7596b861cde0636b8
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-03-08 21:33:33 -0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,14 @@
-var date = moment().format("dddd, MMMM Do YYYY, HHmm");
+var hour = moment().format("dddd, MMMM Do YYYY, HHmm");
 var currentDay = document.getElementById("currentDay");
-var present = document.getElementById("present");
-var past = document.getElementById("past");
-var future = document.getElementById("future");
 var container = document.getElementById("container");
-var currentHour = (currentDay.textContent = date);
 
-//changes color of each timeblock depending on the time of the user visits site
-function colorTimeBlock(hour) {
-  var blockBackground;
-  if (currentHour === hour) {
-    blockBackground === "present";
-  } else if (currentHour > hour) {
-    blockBackground === "past";
-  } else if (currentHour < hour) {
-    blockBackground === "future";
-  }
+var currentHour = (currentDay.textContent = hour);
+
+function updateClock(){
+    var time= moment().format("dddd, MMMM Do YYYY, HHmm");
+    $(currentDay).text(time);
 }
+setInterval(updateClock, 1000);
 
 //function to iterate time, text area, and button
 function buildTimeBlocks() {
@@ -24,29 +16,47 @@ function buildTimeBlocks() {
     var hour = document.createElement("div");
     var textArea = document.createElement("textarea");
     var button = document.createElement("button");
-    //shows time attached to each block
-    hour.innerHTML = (i+ 7).toString().padStart(2, "0").padEnd(4, "0");
-    document.getElementById("hour")
+    //shows time attached to each block, displays 4 digit military time
+    hour.innerHTML = (i + 7).toString().padStart(2, "0").padEnd(4, "0");
+    document.getElementById("hour");
+    hour.classList.add("hour");
     container.appendChild(hour);
     //provides text area
     textArea.innerHTML = "";
-    document.getElementById("textarea")
+    // document.getElementById("textarea");
+    textArea.classList.add("textarea");
     container.appendChild(textArea);
     //provides save btn
     button.innerHTML = "Save";
-    document.getElementById("saveBtn")
+    // document.getElementById("saveBtn");
+    button.classList.add("saveBtn");
     container.appendChild(button);
   }
+  //   var blockBackground = document.createElement("blockBackground");
+  var present = document.createAttribute("id");
+  present.value = "present";
+  var past = document.createAttribute("id");
+  past.value = "past";
+  var future = document.createAttribute("id");
+  future.value = "future";
+  if (currentHour === hour) {
+    textArea.setAttribute("id", present);
+  } else if (currentHour > hour) {
+    textArea.setAttribute("id", past);
+  } else if (currentHour < hour) {
+    textArea.setAttribute("id", future);
+  }
+
+    var saveBtn = button.on("click", function(event) {
+    event.preventDefault();
+    localStorage.setItem(textArea, JSON.stringify(textArea));
+    alert("Information saved.")
+    });
 }
 buildTimeBlocks();
 
-// var save = saveBtn.addEventListener("click", function(event) {
-//     event.preventDefault();
 
-//     localStorage.setItem("var", JSON.stringify(var));
-//     alert("Information saved.")
 
-//     });
 
 
 // a daily planner to create a schedule
@@ -55,3 +65,31 @@ buildTimeBlocks();
 //click into a timeblock I can enter an event
 // click the save button for that timeblock the text for that event is saved in local storage
 //refresh the page, the saved events persist
+
+
+
+
+
+
+
+
+
+
+//changes color of each timeblock depending on the time of the user visits site
+// function colorTimeBlock(hour) {
+//   var blockBackground;
+//   var present = document.createAttribute("present");
+//   var past = document.createAttribute("past");
+//   var future = document.createAttribute("future");
+//   if (currentHour === hour) {
+//     blockBackground === "present";
+//   } else if (currentHour > hour) {
+//     blockBackground === "past";
+//   } else if (currentHour < hour) {
+//     blockBackground === "future";
+//   }
+// }
+// colorTimeBlock();
+// var present = document.getElementById("present");
+// var past = document.getElementById("past");
+// var future = document.getElementById("future");
