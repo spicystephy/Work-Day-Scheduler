03d3c712893be817dedc2a501089799911e644b5
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-03-09 00:02:22 -0700

    final push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-var hour = moment().format("dddd, MMMM Do YYYY, HHmm");
+var currentTime = moment().format("dddd, MMMM Do YYYY, HHmm");
 var currentDay = document.getElementById("currentDay");
 var container = document.getElementById("container");
 
-var currentHour = (currentDay.textContent = hour);
+var currentHour = (currentDay.textContent = currentTime);
 
-function updateClock(){
-    var time= moment().format("dddd, MMMM Do YYYY, HHmm");
-    $(currentDay).text(time);
+function updateClock() {
+  var time = moment().format("dddd, MMMM Do YYYY, HHmm");
+  $(currentDay).text(time);
 }
 setInterval(updateClock, 1000);
 
@@ -16,42 +16,46 @@ function buildTimeBlocks() {
     var hour = document.createElement("div");
     var textArea = document.createElement("textarea");
     var button = document.createElement("button");
+
     //shows time attached to each block, displays 4 digit military time
     hour.innerHTML = (i + 7).toString().padStart(2, "0").padEnd(4, "0");
-    document.getElementById("hour");
+    // document.getElementById("hour");
     hour.classList.add("hour");
     container.appendChild(hour);
+
     //provides text area
     textArea.innerHTML = "";
     // document.getElementById("textarea");
     textArea.classList.add("textarea");
     container.appendChild(textArea);
+
     //provides save btn
     button.innerHTML = "Save";
     // document.getElementById("saveBtn");
     button.classList.add("saveBtn");
     container.appendChild(button);
-  }
-  //   var blockBackground = document.createElement("blockBackground");
-  var present = document.createAttribute("id");
-  present.value = "present";
-  var past = document.createAttribute("id");
-  past.value = "past";
-  var future = document.createAttribute("id");
-  future.value = "future";
-  if (currentHour === hour) {
-    textArea.setAttribute("id", present);
-  } else if (currentHour > hour) {
-    textArea.setAttribute("id", past);
-  } else if (currentHour < hour) {
-    textArea.setAttribute("id", future);
-  }
 
-    var saveBtn = button.on("click", function(event) {
-    event.preventDefault();
-    localStorage.setItem(textArea, JSON.stringify(textArea));
-    alert("Information saved.")
+    //color backgrounds of textareas according to time of day
+    var presentAtt = document.createAttribute("id");
+    presentAtt.value = "present";
+    var pastAtt = document.createAttribute("id");
+    pastAtt.value = "past";
+    var futureAtt = document.createAttribute("id");
+    futureAtt.value = "future";
+    if (currentTime == hour) {
+      textArea.setAttribute("id", "present");
+    } else if (currentTime > hour) {
+      textArea.setAttribute("id", "past");
+    } else if (currentTime < hour) {
+      textArea.setAttribute("id", "future");
+    }
+
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
+      localStorage.setItem(this.textContent, event);
+      alert("Information saved.");
     });
+  }
 }
 buildTimeBlocks();
 
@@ -59,22 +63,13 @@ buildTimeBlocks();
 
 
 
-// a daily planner to create a schedule
-//scroll down, presented with timeblocks for standard business hours
-//each timeblock is color coded to indicate whether it is in the past, present, or future
-//click into a timeblock I can enter an event
-// click the save button for that timeblock the text for that event is saved in local storage
-//refresh the page, the saved events persist
 
 
 
 
 
 
-
-
-
-
+//notes:
 //changes color of each timeblock depending on the time of the user visits site
 // function colorTimeBlock(hour) {
 //   var blockBackground;
